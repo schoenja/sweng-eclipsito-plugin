1c508f99e74f810160c33694e790771421d87663
schoenja <schoenja@users.noreply.github.com>
2017-11-27 10:36:38 +0100

    More cleaning up

--- src/main/kotlin/unibas/sweng/eclipsito/EclipsitoPluginExtension.kt
+++ src/main/kotlin/unibas/sweng/eclipsito/EclipsitoPluginExtension.kt
@@ -40,11 +40,6 @@ open class EclipsitoPluginExtension() {
                     data[point.key] = extension[point.key]
                 } else {
                     if (point.value.first) throw MissingArgumentException("Missing key: ${point.key}.")
-//                    if (!point.value.first) {
-//                        data[point.key] = null
-//                    } else {
-//                        throw MissingArgumentException("Missing key: ${point.key}.")
-//                    }
                 }
             }
 
@@ -67,31 +62,4 @@ open class EclipsitoPluginExtension() {
         }
         this.extPoints.add(ExtensionPoint(pointId, pointName, pointXml, data))
     }
-
-    //lateinit var project: Project
-    /*var extPoints: LinkedHashMap<String, String> = LinkedHashMap<String, String>()
-    var exts: LinkedHashMap<String, MutableList<String>> = LinkedHashMap<String, MutableList<String>>()
-
-
-    fun extension(extension: LinkedHashMap<String, String>) {
-        val extPoint = extension["point"]
-        val extClassname = extension["classname"]
-        if (extPoint != null && extClassname != null) {
-            if (this.exts.containsKey(extPoint)) {
-                this.exts[extPoint]!!.add(extClassname) // !! is safe here as we check whether the point exists.
-                println("Added extension")
-            } else {
-                System.out.println("You need to define the extension point first!")
-            }
-        }
-    }
-
-    fun extensionpoint(extPoint: LinkedHashMap<String, String>) {
-        val pointId = extPoint["id"]
-        val pointName = extPoint["name"]
-        if (pointId != null && pointName != null) {
-            this.extPoints.put(pointId,pointName)
-            this.exts.put(pointName, ArrayList<String>())
-        }
-    }*/
 }
